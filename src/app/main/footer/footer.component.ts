2ad1178e6fd704ab8158ac888f6e0a437e1f9bf1
Toscanelli_MAC <toscanelli_mac@Toscanelli-MACs-MacBook-Pro.local>
2017-10-24 16:21:07 +0200

    footer added

--- src/app/main/footer/footer.component.ts
+++ src/app/main/footer/footer.component.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -8,12 +9,16 @@ import { Component, OnInit } from '@angular/core';
 export class FuseFooterComponent implements OnInit
 {
 
-    constructor()
+    constructor(private router:Router)
     {
     }
 
     ngOnInit()
     {
+    }
+
+    goToFaq(){
+        this.router.navigate(['pages/faq']);
     }
 
 }
